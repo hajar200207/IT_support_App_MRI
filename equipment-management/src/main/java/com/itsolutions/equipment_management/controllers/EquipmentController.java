962c9b558f655b1cccbde1f6e30c2b82ebc84f30
hajar ouadi <ouadihajar2002@gmail.com>
2024-08-06 20:48:35 +0100

    terminer test gestion equipment

--- equipment-management/src/main/java/com/itsolutions/equipment_management/controllers/EquipmentController.java
+++ equipment-management/src/main/java/com/itsolutions/equipment_management/controllers/EquipmentController.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+//terminer test gestion equipment
 @RestController
 @RequestMapping("/api/equipment")
 public class EquipmentController {
